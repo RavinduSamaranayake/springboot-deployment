ba3e4b32d523b5fc8565460dd6ce6f23cf76524e
Ravindu95 <kushan.r@ustocktrade.com>
2022-09-19 15:01:09 +0530

    Added test

--- src/test/java/com/kushan/springbootdeployment/SpringbootDeploymentApplicationTests.java
+++ src/test/java/com/kushan/springbootdeployment/SpringbootDeploymentApplicationTests.java
@@ -1,13 +1,17 @@
 package com.kushan.springbootdeployment;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class SpringbootDeploymentApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("test cases executed..................");
+		assert(true);
 	}
 
 }
