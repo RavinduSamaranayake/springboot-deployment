844a5fa0af7e5462e13e750e54f84166dcd2db64
Ravindu95 <kushan.r@ustocktrade.com>
2022-10-12 10:24:27 +0530

    Added DeploymentController

--- src/main/java/com/kushan/springbootdeployment/DeploymentController.java
+++ src/main/java/com/kushan/springbootdeployment/DeploymentController.java
@@ -1,5 +1,6 @@
 package com.kushan.springbootdeployment;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
  * 10/12/2022
  */
 @RestController
+@Slf4j
 @RequestMapping("/api")
 public class DeploymentController {
     @GetMapping
     public ResponseEntity<String> getAll(){
+        log.info("Controller called.....");
         return ResponseEntity.ok("Deployment Success.....");
     }
 }
